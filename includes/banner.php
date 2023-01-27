df577d119fed6f679f84d64b2bc1cadff1736199
joelmayol0895 <82931750+joelmayol0895@users.noreply.github.com>
2023-01-27 17:33:19 +0800

    my first commit

--- includes/banner.php
+++ includes/banner.php
@@ -11,7 +11,7 @@
             <div class="about-content-wrapper">
 
                 <div class="about-img">
-                    <img src="images/profile.jpg" alt="profile picture">
+                    <img src="images/profile.jpg" alt="profile">
                 </div>
 
                 <div class="about-me">
